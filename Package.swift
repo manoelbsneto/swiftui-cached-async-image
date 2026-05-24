ec345258830c5e13668fca7c22aab7f5113ccfa3
manoelbsneto <manoelbsbeto@gmail.com>
2026-05-24 01:09:25 -0300

    fix: remove visionOS platform (not supported in swift-tools-version 5.6)

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.9
+// swift-tools-version:5.6
 
 import PackageDescription
 
@@ -8,8 +8,7 @@ let package = Package(
         .iOS(.v13),
         .macOS(.v10_15),
         .tvOS(.v13),
-        .watchOS(.v6),
-        .visionOS(.v1)
+        .watchOS(.v6)
     ],
     products: [
         .library(
@@ -22,7 +21,6 @@ let package = Package(
 )
 
 #if swift(>=5.6)
-// Add the documentation compiler plugin if possible
 package.dependencies.append(
     .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
 )
